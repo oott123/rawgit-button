1e06b6d0052576c9ced017cbcdf90ef310f8b1ae
三三 <oott123@users.noreply.github.com>
2017-02-02 17:02:55 +0800

    add pjax support

--- rawgit-button.user.js
+++ rawgit-button.user.js
@@ -1,16 +1,23 @@
 // ==UserScript==
 // @name        RawGit Button for Github.com
 // @namespace   oott123_raw_git_button_for_github
-// @include     https://github.com/*/*/blob/*
-// @version     1
+// @include     https://github.com/*
+// @version     2
 // @grant       none
 // ==/UserScript==
 
-var rawDom = document.querySelector('#raw-url');
-var url = rawDom.href.replace(/(([^\/]+\/){2})raw\//, (a, b) => b).replace(/^https:\/\/github\.com\//, 'https://rawgit.com/');
-var rawGitDom = document.createElement('a');
-rawGitDom.href = url;
-rawGitDom.textContent = 'RawGit';
-rawGitDom.className = 'btn btn-sm BtnGroup-item';
-rawGitDom.target = '_blank';
-rawDom.parentNode.insertBefore(rawGitDom, rawDom.nextSibling);
+document.addEventListener('pjax:complete', rawgitButton);
+
+function rawgitButton() {
+    var rawDom = document.querySelector('#raw-url');
+    if (!rawDom) {
+        return;
+    }
+    var url = rawDom.href.replace(/(([^\/]+\/){2})raw\//, (a, b) => b).replace(/^https:\/\/github\.com\//, 'https://rawgit.com/');
+    var rawGitDom = document.createElement('a');
+    rawGitDom.href = url;
+    rawGitDom.textContent = 'RawGit';
+    rawGitDom.className = 'btn btn-sm BtnGroup-item';
+    rawGitDom.target = '_blank';
+    rawDom.parentNode.insertBefore(rawGitDom, rawDom.nextSibling);
+}
